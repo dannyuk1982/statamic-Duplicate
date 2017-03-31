0d4ed0cdce576eb9ca9a061693df5595e9bd48a4
Danny Richardson <danny@Dannys-Mac-Pro.local>
2017-03-31 19:10:09 +0100

    Revert "Update addButtons.js"
    This reverts commit 4d75a826ba7e0c9cefffc1a3c559daf0070e84a5.

--- resources/assets/js/addButtons.js
+++ resources/assets/js/addButtons.js
@@ -1,38 +1,34 @@
 var addButtons = function($) {
 
-  // label to use for the duplicate button - should be translatable really
+  //lavel to use for the duplicate button - should be translatable really
   var label = 'Duplicate';
 
-  // check we are on an entry index page...
  	if( location.href.indexOf( 'collections/entries' ) !== -1 ) {
 
-    //get the collection we are on
+    //get the collection
     var collection = location.href.split('/');
     collection.splice( 0, collection.length - 1);
 
-    // add the buttons (this would be probably be better done via Vue?)
    	$('ul.dropdown-menu').each( function() {
 
-      // get the slug
+      //get the slug and link
       var slug = $(this).parents('td').siblings('.cell-slug').html().trim();
-      
-      // get the link to the controller
       var href = '/cp/addons/duplicateentry/' + collection + '/' + slug;
 
-      // create an element to add
+      //create an element to add
      	var li = $('<li><a href="' + href + '">' + label + '</a></li>');
 
-      // add to the DOM, after the 'edit' button, it could be after 'delete' if preferred, change eq(0) to eq(1)
+      //add to the DOM
      	$(this).children('li').eq(0).after( li );
 
    	});
 
  	}
-  
+
 }
 
-// call this a bit after the window has loaded (give some time for Vue to load) -
-// really this should be called after Vue has done it's thing
+//call this a bit after the window has loaded (give some time for vue to load) -
+//really this should be called after vue has done it's thing
 window.addEventListener("load", function(event) {
 
   setTimeout(function() { addButtons( jQuery ); }, 500 )
