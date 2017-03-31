51a049dc06542ab955b01d664a90cfd6bd5f3d94
Danny Richardson <danny@Dannys-Mac-Pro.local>
2017-03-31 19:12:43 +0100

    Tidied up indentation

--- resources/assets/js/addButtons.js
+++ resources/assets/js/addButtons.js
@@ -4,14 +4,14 @@ var addButtons = function($) {
   var label = 'Duplicate';
 
   // check we are on an entry index page...
- 	if( location.href.indexOf( 'collections/entries' ) !== -1 ) {
+  if( location.href.indexOf( 'collections/entries' ) !== -1 ) {
 
     //get the collection we are on
     var collection = location.href.split('/');
     collection.splice( 0, collection.length - 1);
 
     // add the buttons (this would be probably be better done via Vue?)
-   	$('ul.dropdown-menu').each( function() {
+    $('ul.dropdown-menu').each( function() {
 
       // get the slug
       var slug = $(this).parents('td').siblings('.cell-slug').html().trim();
@@ -20,19 +20,20 @@ var addButtons = function($) {
       var href = '/cp/addons/duplicateentry/' + collection + '/' + slug;
 
       // create an element to add
-     	var li = $('<li><a href="' + href + '">' + label + '</a></li>');
+      var li = $('<li><a href="' + href + '">' + label + '</a></li>');
 
       // add to the DOM, after the 'edit' button, it could be after 'delete' if preferred, change eq(0) to eq(1)
-     	$(this).children('li').eq(0).after( li );
+      $(this).children('li').eq(0).after( li );
 
-   	});
+    });
 
- 	}
+  }
   
 }
 
 // call this a bit after the window has loaded (give some time for Vue to load) -
-// really this should be called after Vue has done it's thing
+// this should be better than just guessing at 500ms and assuming Vue is done,
+// NOTE: this is also untested with pagination
 window.addEventListener("load", function(event) {
 
   setTimeout(function() { addButtons( jQuery ); }, 500 )
